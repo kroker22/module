6fb637c8dcb319261a2eae21f2acaa931139a741
hoperes2 <hoperes232@naver.com>
2024-01-15 11:26:46 +0900

    comment delete

--- frameworks.py
+++ frameworks.py
@@ -85,7 +85,7 @@ class combo_make() :
             return self.box_name_init.get()
         
         
-class check_make():
+class check_make(combo_make):
     def __init__(self):
         super().__init__()
         self.Digital_Pin = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
@@ -95,47 +95,13 @@ class check_make():
     def check_(self, using_combo):
         self.check_boxes = []  # Reset the list for each call
         pins = self.Digital_Pin if using_combo.getU("pin_") == "digital" else self.Analog_Pin
+        # 삼항 연산자로 pin 에 데이터 저장
 
         for i, pin in enumerate(pins):
             var = tk.IntVar()
             check_box = tk.Checkbutton(root, text=str(pin), variable=var)
             check_box.place(x=10, y=170 + i * 20)
             self.check_boxes.append(var)
-
-"""
-class check_make(combo_make) :
-    ## Using inheritance, make check box class
-    def __init__ (self ) : 
-        super().__init__() 
-        self.Digital_Pin = [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
-        self.Analog_Pin = ["A0","A1","A2","A3","A4","A5"]
-        self.temp = []
-        self.checking = []
-    def check_(self,using_combo):  
-        # if annotion string is digial 
-        # make type of variable " INT " i for checkbox indexes
-       
-
-        self.temp = self.Digital_Pin
-        self.checking = using_combo.getU("pin_")
-        for i in range(len(self.Digital_Pin)) :
-            globals()['check_box_{}'.format(i)] = self.Digital_Pin[i]
-          
-            
-            print(globals().keys())
-  
-            # check_box1 = 0
-            # check_box2 = 1
-            
-
-check_var1 = IntVar()
-c1 = Checkbutton(root, text='digital', variable=check_var1)
-c1.place(x=10, y = 200)
-        
-
-"""
-
-     
                 
                 
 #############################
